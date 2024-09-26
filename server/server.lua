5cbfbdfe25c3ab72a6a95e16e9edd5b44f2fad74
RexshackGaming <rexshackuk@gmail.com>
2024-09-26 16:27:21 +0100

    version checker

--- server/server.lua
+++ server/server.lua
@@ -1,37 +1,5 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
------------------------------------------------------------------------
--- version checker
------------------------------------------------------------------------
-local function versionCheckPrint(_type, log)
-    local color = _type == 'success' and '^2' or '^1'
-
-    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
-end
-
-local function CheckVersion()
-    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-medic/main/version.txt', function(err, text, headers)
-        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
-
-        if not text then 
-            versionCheckPrint('error', 'Currently unable to run a version check.')
-            return 
-        end
-
-        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
-        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
-        
-        if text == currentVersion then
-            versionCheckPrint('success', 'You are running the latest version.')
-        else
-            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
-        end
-    end)
-end
-
------------------------------------------------------------------------
-
-
 -- Admin Revive Player
 RSGCore.Commands.Add("revive", Lang:t('server.lang_1'), {{name = "id", help = Lang:t('server.lang_2')}}, false, function(source, args)
     local src = source
@@ -179,8 +147,3 @@ RSGCore.Functions.CreateCallback('rsg-medic:server:getmedics', function(source, 
     end
     cb(amount)
 end)
-
---------------------------------------------------------------------------------------------------
--- start version check
---------------------------------------------------------------------------------------------------
-CheckVersion()
